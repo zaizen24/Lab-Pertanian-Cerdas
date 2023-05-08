fceb7f86082b6e4f3454efcc38b03427e5b80b9a
zaizen24 <116472858+zaizen24@users.noreply.github.com>
2023-05-08 16:11:57 +0700

    Update sketch.ino

--- Sensor LDR Cahaya (Wokwi)/sketch.ino
+++ Sensor LDR Cahaya (Wokwi)/sketch.ino
@@ -1,21 +1,21 @@
-int led1=13;
-int cahaya=12;
+int led1=13; // inisialisasi led1 berada di pin 13
+int cahaya=12; // inisialisasi cahaya atau sensor ldr berada di pin 12
 void setup() {
   // put your setup code here, to run once:
-pinMode (led1,OUTPUT);
-pinMode(cahaya,INPUT);
-Serial.begin(9600);
+pinMode (led1,OUTPUT); // mengubah pin mode led1 menjadi output
+pinMode(cahaya,INPUT); // mengubah pin mode cahaya menjadi input
+Serial.begin(9600); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-int data = analogRead(cahaya);
-Serial.println(data);
+int data = analogRead(cahaya); // inisialisai data sama dengan membaca sensor cahaya
+Serial.println(data); // menampilkan data di serial monitor dengan baris per baris
 
-if (data > 500){
+if (data > 0){ // jika sensor tidak mendeteksi cahaya maka led akan hidup
   digitalWrite(led1, HIGH);
 }
-else{
+else{ // jika ada cahaya maka led akan mati
   digitalWrite(led1, LOW);
 }
 
